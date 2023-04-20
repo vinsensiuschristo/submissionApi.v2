e7b51aaacae0b3f084017c6da0c01e1c8706ba04
vinsensiuschristo <hallavinsen@gmail.com>
2023-04-20 13:01:32 +0700

    authentication done

--- src/service/postgres/UsersService.js
+++ src/service/postgres/UsersService.js
@@ -2,6 +2,8 @@ const { nanoid } = require("nanoid");
 const { Pool } = require("pg");
 const bcrypt = require("bcrypt");
 const InvariantError = require("../../exceptions/InvariantError");
+// const NotFoundError = require("../../exceptions/NotFoundError");
+const AuthenticationError = require("../../exceptions/AuthenticationError");
 
 class UsersService {
     constructor() {
@@ -39,6 +41,29 @@ class UsersService {
                 "Gagal menambahkan user. Username sudah digunakan."
             );
         }
+    }
+
+    async verifyUserCredential(username, password) {
+        const query = {
+            text: "SELECT id, password FROM users WHERE username = $1",
+            values: [username],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new AuthenticationError("Kredensial yang Anda berikan salah");
+        }
+
+        const { id, password: hashedPassword } = result.rows[0];
+
+        const match = await bcrypt.compare(password, hashedPassword);
+
+        if (!match) {
+            throw new AuthenticationError("Kredensial yang Anda berikan salah");
+        }
+
+        return id;
     }
 }
 
